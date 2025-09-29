57f134149c8d620d5a18ea3c1c6388e78dc25dd3
suagrtts <kiethdaryll1@gmail.com>
2025-09-29 20:25:23 +0800

    Delete LigaOlympica directory

--- LigaOlympica/src/main/java/ligaolympica/Main.java
+++
@@ -1,8 +0,0 @@
-
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Liga Olympica!");
-    }
-}
-dili ko kita sa files
--- LigaOlympica/src/main/java/ligaolympica/character/ChronosOP.java
+++
@@ -1,5 +0,0 @@
-package main.java.ligaolympica.character;
-
-public class ChronosOP {
-
-}
